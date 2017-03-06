536785521c9f30c62b8d9cbca2ef6e0ef6724c17
Алексей <alex-malina_47@inbox.ru>
2017-03-06 21:21:09 +0300

    Lw7.1

---
+++ Lw7.1/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool check_polindrom(string word)
+{
+	int len = word.length();
+	for (int i = 0; i < len / 2; ++i)
+	{
+		if (word[i] != word[len - i - 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter the word: ";
+	cin >> str;
+	if (check_polindrom(str))
+	{
+		cout << "Word is polindrom.";
+	}
+	else
+	{
+		cout << "Word is not polindrom";
+	}
+	system("pause");
+	return 0;
+}
+
+
+
