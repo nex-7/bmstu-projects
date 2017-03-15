974142c826b022c6995680fd2bd911e1a57e4325
Алексей <alex-malina_47@inbox.ru>
2017-03-15 18:53:40 +0300

    M2-Lw1

--- M2-Lw1/Source.cpp
+++ M2-Lw1/Source.cpp
@@ -3,31 +3,10 @@
 #include "CastException.h"
 
 int intFromChar(char data) {
-    switch (data)
-    {
-    case '1':
-        return 1;
-    case '2':
-        return 2;
-    case '3':
-        return 3;
-    case '4':
-        return 4;
-    case '5':
-        return 5;
-    case '6':
-        return 6;
-    case '7':
-        return 7;
-    case '8':
-        return 8;
-    case '9':
-        return 9;
-    case '0':
-        return 0;
-    default:
-        throw ErrorChar();
-    }
+	if ('0' <= data && data <= '9')
+		return data - '0';
+	else
+		throw ErrorChar();
 }
 
 bool boolFromString(const char * data) {
