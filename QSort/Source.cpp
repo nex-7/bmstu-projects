3aaeb4eb1e3e01b22ceddd029e43f78e6dd5da42
nex-7 <alex-malina_47@inbox.ru>
2017-06-11 16:04:09 +0300

    Update Source.cpp

--- QSort/Source.cpp
+++ QSort/Source.cpp
@@ -1,52 +1,29 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
 template<class It, class Cmp>
-It Srt(It beg, It end, Cmp cmp)
-{
-	It position = beg;
-	while (1)
-	{
-		It iter = end;
-		while (cmp(*position, *iter) && (iter > position))
-			--iter;
-		if (iter != position)swap(*position, *iter);
-		else
-		{
-			return position;
-			break;
-		}
-		position = iter;
-		iter = beg;
-		++iter;
-		while (cmp(*iter, *position) && (iter < position))
-			++iter;
-		if (iter != position) swap(*position, *iter);
-		else
-		{
-			return position;
-			break;
-		}
-		position = iter;
-	}
-}
-template<class It, class Cmp>
+
 void qsort(It beg, It end, Cmp cmp)
 {
-	It i = end;
-	--end;
-	It p = Srt<It, Cmp>(beg, end, cmp);
-	It Next = p;
-	if (Next != end) ++Next;
-	if (distance(beg, p)>0) qsort<It, Cmp>(beg, p, cmp);
-	if (distance(Next, end)>0) qsort<It, Cmp>(Next, i, cmp);
+	It left = beg;
+	It tmp = left++;
+	It right = end;
+	while (left != right)
+	{
+		if (cmp(*left, *tmp)) 
+			++left;
+		else 
+		{
+			while ((left != --right) && cmp(*tmp, *right));
+			std::iter_swap(left, right);
+		}
+	}
+	std::iter_swap(beg, --left);
+	if (std::distance(beg, left)>0)
+		qsort(beg, left, cmp);
+	if (std::distance(right, end)>0)
+		qsort(right, end, cmp);
 }
-void main()
-{
-	vector<int> buffer;
-	for (unsigned int i = 0; i < 15; ++i) buffer.push_back(rand() % 100);
-	qsort<vector<int>::iterator, less<int>>(buffer.begin(), buffer.end(), less<int>());
-	for (unsigned int i = 0; i < 15; ++i) cout << buffer[i] << endl;
-	system("pause");
-	return;
-}
+
