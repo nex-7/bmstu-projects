47af08be2d41d3a7ee75a883cdeb93043f23b582
nex-7 <alex-malina_47@inbox.ru>
2017-06-12 17:47:33 +0300

    Update main.cpp

--- Lw6/main.cpp
+++ Lw6/main.cpp
@@ -1,6 +1,14 @@
 #include <thread>
 #include "zooList.h"
 
+enum TAnimalType {
+    TGiraffaType = 0
+    , THippopotamusType
+    , TElephasType
+    , TLoxodontaType
+    , TLeoType
+    , TTigrisType
+};
 
 int switchAnimals(TZoo & zoo1,int i1, TZoo & zoo2, int i2){
     zoo1.AddNewAnimal(&zoo2[i2]);
@@ -10,13 +18,38 @@ int switchAnimals(TZoo & zoo1,int i1, TZoo & zoo2, int i2){
 	return 0;
 }
 
+TAnimal * createAnimal(TAnimalType type, std::string name = "Name")
+{
+    if (type == TGiraffaType) {
+        return new TGiraffa(name);
+    }
+    else if (type == THippopotamusType) {
+        return new THippopotamus(name);
+    }
+    else if (type == TElephasType) {
+        return new TElephas(name);
+    }
+    else if (type == TLoxodontaType) {
+        return new TLoxodonta(name);
+    }
+    else if (type == TLeoType) {
+        return new TLeo(name);
+    }
+    else if (type == TTigrisType) {
+        return new TTigris(name);
+    }
+    else {
+        return nullptr;
+    }
+}
+
 int main() {
     TZoo myZoo_first(288);
     TZoo myZoo_second(288);
-    TAnimal *tiger = new TTigris("pasha");
-    TAnimal *second_tiger = new TTigris("petya");
-    TAnimal *slon = new TElephas("vasya");
-    TAnimal * gip = new THippopotamus("vera");
+    TAnimal *tiger = createAnimal(TTigrisType, "pasha");
+    TAnimal *second_tiger = createAnimal(TTigrisType, "petya");
+    TAnimal *slon = createAnimal(TElephasType, "vasya");
+    TAnimal * gip = createAnimal(TGiraffaType, "vera");
     if (!myZoo_first.AddNewAnimal(&tiger))
         delete tiger;
     if (!myZoo_first.AddNewAnimal(&second_tiger))
@@ -25,6 +58,7 @@ int main() {
         delete slon;
     if (!myZoo_second.AddNewAnimal(&gip))
         delete gip;
+
     for (size_t i = 0; i < 100; ++i) 
 	{
         myZoo_first.Work();
@@ -34,14 +68,14 @@ int main() {
         }
         std::cout << std::endl;
 
-
-
-
-
-
-
-
-
+        if (i % 10 == 0) {
+            std::string name;
+            std::cout << "Введите имя: ";
+            std::cin >> name;
+            TAnimal * animal = createAnimal(TAnimalType(rand() % 6), name);
+            if (!myZoo_first.AddNewAnimal(&animal))
+                delete animal;
+        }
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
