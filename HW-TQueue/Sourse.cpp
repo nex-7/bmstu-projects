8b5d2f3efebcac7a2b15018fae318c38c1bb9a93
nex-7 <alex-malina_47@inbox.ru>
2017-09-11 22:52:58 +0300

    Update Sourse.cpp

--- HW-TQueue/Sourse.cpp
+++ HW-TQueue/Sourse.cpp
@@ -1,40 +1,70 @@
+#include "../simple_memory_leak_detector.h"
+
+#include <queue>
 #include <iostream>
-#include "TQueue.h"
-​
-int main() {
-    TQueue<int> q;
-    std::cout << "q.empty:" << q.empty() << std::endl;
-    std::cout << "q.size:" << q.size() << std::endl;
-    std::cout << "q.push(1, 2, 3); q.pop();" << std::endl;  q.push(1); q.push(2); q.push(3); q.pop();
-    std::cout << "q.print: "; q.print();
-    std::cout << "q.empty:" << q.empty() << std::endl;
-    std::cout << "q.size:" << q.size() << std::endl;
-    std::cout << "q.front:" << q.front() << std::endl;
-    std::cout << "q.back:" << q.back() << std::endl << std::endl;
-​
-    TQueue<int> q2;
-    std::cout << "q2.empty: " << q2.empty() << std::endl;
-    std::cout << "q2.size: " << q2.size() << std::endl;
-    std::cout << "q2.swap(q); q2.pop();" << std::endl; q2.swap(q); q2.pop();
-    std::cout << "q.empty: " << q.empty() << std::endl;
-    std::cout << "q.size: " << q.size() << std::endl;
-    std::cout << "q2.print: "; q2.print();
-    std::cout << "q2.empty: " << q2.empty() << std::endl;
-    std::cout << "q2.size: " << q2.size() << std::endl;
-    std::cout << "q2.front: " << q2.front() << std::endl;
-    std::cout << "q2.back: " << q2.back() << std::endl;
-    std::cout << "q2.pop();" << std::endl; q2.pop();
-    std::cout << "q2.print: "; q2.print();
-    std::cout << "q2.empty: " << q2.empty() << std::endl;
-    std::cout << "q2.size: " << q2.size() << std::endl;
-​
-    try {
-        std::cout << "q2.front: " << q2.front() << std::endl;
-    }
-    catch (std::exception& e) {
-        std::cout << e.what() << std::endl;
-    }
-​
-    system("pause");
-    return 0;
+#include <string>
+
+#define CHECK
+
+#ifdef CHECK
+#include "test_queue.h"
+
+template<class T>
+using queue = TQueue<T>;
+
+#else
+
+template<class T>
+using queue = std::queue<T>;
+
+#endif // CHECK
+
+
+#define assert(X) { \
+    if(!(X)) {\
+        std::cout << "see function:'" << __FUNCTION__ << "' line: " << __LINE__;\
+        throw std::logic_error(#X); \
+    }\
+}
+
+int main()
+{
+   {
+       queue<std::string> m;
+       assert(m.empty());
+
+       m.push("a");
+       m.push("b");
+       m.push("c");
+       assert(m.size() == 3);
+
+       assert(m.front() == "a");
+       assert(m.front() == "a");
+       assert(m.front() == "a");
+
+       auto m1 = m;
+       assert(m.front() == "a");
+
+       m1.pop();
+       m1.pop();
+       m1.pop();
+       assert(m1.empty());
+
+       m1.swap(m);
+       assert(m.empty());
+       assert(m1.size() == 3);
+
+       assert(m1.back() == "c");
+       
+       m1.pop();
+       assert(m1.front() == "b");
+
+       m1.pop();
+       assert(m1.front() == "c");
+
+       m1.pop();
+       assert(m1.empty());
+   }
+
+   std::cout << std::boolalpha << memoryLeakExist() << std::endl;
 }
